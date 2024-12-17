ec923a7bf6d1aeeceea3d3814f576dd7239c34c9
draindoggy <119752646+draindoggy@users.noreply.github.com>
2024-12-17 18:14:46 +0300

    Update mysql_db_server.py

--- mysql_db_server.py
+++ mysql_db_server.py
@@ -13,15 +13,26 @@ db_config = {
 def get_db_connection():
     return mysql.connector.connect(**db_config)
 
-@app.route('/')
-def home():
-    return "Welcome to the Flask'trofim app!"
-
 @app.route('/data', methods=['GET'])
 def get_table_data():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM Teachers")
+    cursor.execute('''
+        SELECT
+            t.FullName,
+            l.day,
+            g.group_name,
+            s.SubjectName,
+            lt.lesson_type,
+            r.RoomNumber'
+        FROM Lesson l
+        JOIN Teachers t ON l.TeacherID = t.id
+        JOIN Groups g ON l.GroupID = g.id
+        JOIN Subjects1 s ON l.SubjectID = s.id
+        LEFT JOIN Lesson_Type1 lt ON l.lesson_type = lt.id
+        JOIN Rooms r ON l.RoomID = r.id
+        WHERE t.id = @TeacherID
+        ORDER BY l.day''')
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
