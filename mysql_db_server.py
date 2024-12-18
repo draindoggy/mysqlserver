29262cc3e3bd6c59130abd56e259c4acd9f6c4f3
draindoggy <119752646+draindoggy@users.noreply.github.com>
2024-12-18 15:50:21 +0300

    done?

--- mysql_db_server.py
+++ mysql_db_server.py
@@ -18,7 +18,7 @@ def home():
     return "welcomeee to the Flask'trofim app!"
 
 @app.route('/data', methods=['GET'])
-def get_table_data():
+def get_teacher_data():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute('''
@@ -36,6 +36,56 @@ def get_table_data():
         LEFT JOIN lesson_type1 lt ON l.lesson_type = lt.id
         JOIN rooms r ON l.RoomID = r.id
         WHERE t.id = 1
+        ORDER BY l.day''')
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+@app.route('/group', methods=['GET'])
+def get_group_data():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('''
+        SELECT
+            g.group_name,
+            t.FullName,
+            l.day,
+            s.SubjectName,
+            lt.lesson_type,
+            r.RoomNumber
+        FROM lesson l
+        JOIN teachers t ON l.TeacherID = t.id
+        JOIN groups g ON l.GroupID = g.id
+        JOIN subjects1 s ON l.SubjectID = s.id
+        LEFT JOIN lesson_type1 lt ON l.lesson_type = lt.id
+        JOIN rooms r ON l.RoomID = r.id
+        WHERE g.id = 1
+        ORDER BY l.day''')
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+@app.route('/room', methods=['GET'])
+def get_room_data():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('''
+        SELECT
+            r.RoomNumber,
+            g.group_name,
+            t.FullName,
+            l.day,
+            s.SubjectName,
+            lt.lesson_type
+        FROM lesson l
+        JOIN teachers t ON l.TeacherID = t.id
+        JOIN groups g ON l.GroupID = g.id
+        JOIN subjects1 s ON l.SubjectID = s.id
+        LEFT JOIN lesson_type1 lt ON l.lesson_type = lt.id
+        JOIN rooms r ON l.RoomID = r.id
+        WHERE r.id = 1
         ORDER BY l.day''')
     rows = cursor.fetchall()
     cursor.close()
